572906c42c339b49ecd51d7ac9d7a16c886e05c4
ALEF BACELAR <74435383+Alefbacelar@users.noreply.github.com>
2023-01-12 23:17:53 -0300

    Exercicios

--- Aulas/code/index.ts
+++ Aulas/code/index.ts
@@ -1,4 +1,4 @@
-console.log('Olá mundo!')
+/* console.log('Olá mundo!')
 
 function add (num1:number, num2:number){
   return num1 + num2
@@ -6,69 +6,159 @@ function add (num1:number, num2:number){
 
 let result = add(5, 6)
 
-console.log(result);
+console.log(result); */
 
 //COMO FUNCIONA O TIPO ANY
 
-let price = 40
+/* let price = 40
 let item = 'Apple'
 let inStore = true
 
 let value
 value = 10
-value = 'Alef'
+value = 'Alef' */
 
 // AS ARRAYS / LISTAS
 // Tipar array number/string
-let values: number[]  = [
-  10,20,30,40,50,
-]
+/* let values: number[]  = [
+  10,20,30,40,50, */
+/* ] */
 
 
 //TUPLES VOCÊ PÕE OS TIPOS NUMBER STRING BOOLEAN
 
-let items:[string,number,boolean]=['apple', 3, true]
+/* let items:[string,number,boolean]=['apple', 3, true] */
 
 //ENUM BOM PARA ASSOCIAR DOIS ITENS
 
-enum Role {
+/* enum Role {
   admin = 1,
   read = 2,
   backup = 2
-}
+} */
 
-const user = {
+/* const user = {
   firstName: 'Alef',
   age: 27,
   role: Role.backup 
-}
+} */
 
-console.log(user)
+/* console.log(user) */
 
 //FUNÇÃO VOID NÃO TEM RETORNO
 
-function resultado(price: number){
+/* function resultado(price: number){
   console.log('O resultado' + price)
-}
+} */
 
 //Objetos
 
-const usuario = {
+/* const usuario = {
   name: 'Alef Bacelar',
   idade: 27,
-}
+} */
 
 // O UNKNOWN parecido com o any, ele pode ser qualquer coisa de uma forma temporaria.
 
-let itemInput: unknown
+/* let itemInput: unknown
 let itemName: string
 
 itemInput = 10
-itemInput = 'agora é string'
+itemInput = 'agora é string' */
 
 //NEVER NÃO PASSA NADA GERA ERRO E PARA APLICAÇÃO
 
-function generateError(message: string, code:number): never{
+/* function generateError(message: string, code:number): never{
   throw {message: message, errorCode: code}
 }
-generateError('A aplicação travou', 500)
+generateError('A aplicação travou', 500); */
+
+// Type ALias
+
+/* type Users = {
+  firstName: string,
+  age: number,
+}
+
+const user: Users = {
+  firstName: 'Alef',
+  age: 27
+}
+console.log(user) */
+
+// Union Type
+
+/* function userInput(input1: number | string, input2: number | string){
+  let result
+  if(typeof input1 === 'number' && typeof input2 === 'number'){
+    result = input1 + input2
+  }
+  else{
+    result = input1.toString() + input2.toString()
+  }
+  return result
+}
+
+const combinePrices = userInput(10,20)
+console.log(combinePrices)
+
+const combineName = userInput('apple ','avocado')
+console.log(combineName) */
+
+
+
+//LITERAL TYPE
+
+/* let productPrice: 10 | 20 | 30;
+
+let productSize: 'S' | 'M' | 'L'
+
+productPrice = 30
+
+productSize = 'S' */
+
+
+
+//INTERSECTION
+//JUNTAR DOIS TYPE 
+
+/* type User = {
+  nome: string
+  idade: number
+}
+
+type funcaoNaEmpresa = {
+  id: number
+  role: string
+}
+
+type incluir = User & funcaoNaEmpresa
+
+const e1: incluir = {
+  nome : 'Alef',
+  idade: 27,
+  id: 221,
+  role: 'Admin'
+} */
+
+
+ /* ********** EXERCICIOS ********** */
+
+ /* Exercicio 1 */
+
+/*  let pi = 3.14159;
+ let tau = pi * 2;
+
+ console.log(`${tau} is ${pi} times two.`); */
+
+ /* Exercicio 2 */
+
+/*  let pie : string
+ pie = 'blueberry'
+
+ console.log(`I like to eat ${pie}-flavored pie.`) */
+
+  /* Exercicio 3 */
+
+/*   let isALef: boolean = true
+  console.log(`${isAlef ? 'Oh, Hi Alef' : 'who are you?'}`) */
--- Aulas/prod/index.js
+++ Aulas/prod/index.js
@@ -1,7 +1,127 @@
 "use strict";
-console.log('Olá mundo!');
-function add(num1, num2) {
-    return num1 + num2;
+/* console.log('Olá mundo!')
+
+function add (num1:number, num2:number){
+  return num1 + num2
 }
-var result = add(5, 6);
-console.log(result);
+
+let result = add(5, 6)
+
+console.log(result); */
+//COMO FUNCIONA O TIPO ANY
+/* let price = 40
+let item = 'Apple'
+let inStore = true
+
+let value
+value = 10
+value = 'Alef' */
+// AS ARRAYS / LISTAS
+// Tipar array number/string
+/* let values: number[]  = [
+  10,20,30,40,50, */
+/* ] */
+//TUPLES VOCÊ PÕE OS TIPOS NUMBER STRING BOOLEAN
+/* let items:[string,number,boolean]=['apple', 3, true] */
+//ENUM BOM PARA ASSOCIAR DOIS ITENS
+/* enum Role {
+  admin = 1,
+  read = 2,
+  backup = 2
+} */
+/* const user = {
+  firstName: 'Alef',
+  age: 27,
+  role: Role.backup
+} */
+/* console.log(user) */
+//FUNÇÃO VOID NÃO TEM RETORNO
+/* function resultado(price: number){
+  console.log('O resultado' + price)
+} */
+//Objetos
+/* const usuario = {
+  name: 'Alef Bacelar',
+  idade: 27,
+} */
+// O UNKNOWN parecido com o any, ele pode ser qualquer coisa de uma forma temporaria.
+/* let itemInput: unknown
+let itemName: string
+
+itemInput = 10
+itemInput = 'agora é string' */
+//NEVER NÃO PASSA NADA GERA ERRO E PARA APLICAÇÃO
+/* function generateError(message: string, code:number): never{
+  throw {message: message, errorCode: code}
+}
+generateError('A aplicação travou', 500); */
+// Type ALias
+/* type Users = {
+  firstName: string,
+  age: number,
+}
+
+const user: Users = {
+  firstName: 'Alef',
+  age: 27
+}
+console.log(user) */
+// Union Type
+/* function userInput(input1: number | string, input2: number | string){
+  let result
+  if(typeof input1 === 'number' && typeof input2 === 'number'){
+    result = input1 + input2
+  }
+  else{
+    result = input1.toString() + input2.toString()
+  }
+  return result
+}
+
+const combinePrices = userInput(10,20)
+console.log(combinePrices)
+
+const combineName = userInput('apple ','avocado')
+console.log(combineName) */
+//LITERAL TYPE
+/* let productPrice: 10 | 20 | 30;
+
+let productSize: 'S' | 'M' | 'L'
+
+productPrice = 30
+
+productSize = 'S' */
+//INTERSECTION
+//JUNTAR DOIS TYPE 
+/* type User = {
+  nome: string
+  idade: number
+}
+
+type funcaoNaEmpresa = {
+  id: number
+  role: string
+}
+
+type incluir = User & funcaoNaEmpresa
+
+const e1: incluir = {
+  nome : 'Alef',
+  idade: 27,
+  id: 221,
+  role: 'Admin'
+} */
+/* ********** EXERCICIOS ********** */
+/* Exercicio 1 */
+/*  let pi = 3.14159;
+ let tau = pi * 2;
+
+ console.log(`${tau} is ${pi} times two.`); */
+/* Exercicio 2 */
+/*  let pie : string
+ pie = 'blueberry'
+
+ console.log(`I like to eat ${pie}-flavored pie.`) */
+/* Exercicio 3 */
+var isALef;
+console.log("" + ('isAlef' ? 'Oh, Hi Alef' : 'who are you?'));
